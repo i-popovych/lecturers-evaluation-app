32d6a883ab347294fa05530f3ec689ee30cd9a01
Vlad <100844370+Vladyslav2503@users.noreply.github.com>
2023-04-11 22:03:20 +0300

    update lists

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from "./pages/Header";
 import AppRouter from "./components/AppRouter";
+import './App.css'
+import Racing from './pages/Racing';
 import ProfessorProfile from './pages/ProfessorProfile';
+import Album from './pages/StudentProfile';
 
 const App = () => {
     return (
         <>
             <Header/>
             <AppRouter/>
-            <ProfessorProfile/>
+            <Racing/>
         </>
     );
 };
