d85a43c35de40cc6ad234cee3716e48dc48ea331
Ivan Popovych <100025369+i-popovych@users.noreply.github.com>
2023-04-12 00:34:37 +0300

    update app2.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, { createContext, useState } from 'react';
-=======
 import React, {createContext, useEffect, useState} from 'react';
->>>>>>> 2746550cc7f562f188f160e7434d6b46664c076b
 import Header from "./pages/Header";
 import AppRouter from "./components/AppRouter";
 import "./App.css"
